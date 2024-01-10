b8780964d1f83615e84c51f63c069aaa71b08841
Asier Salguero <asier.salguero@gmail.com>
2024-01-10 20:58:30 +0100

    AWDasd

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    drafts: true,
+    shikiConfig: {
+      theme: "css-variables"
+    }
+  },
+  shikiConfig: {
+    wrap: true,
+    skipInline: false,
+    drafts: true
+  },
+  site: 'https://farfuflis.github.io',
+  integrations: [tailwind(), sitemap(), mdx()],
+  base: '/',
+});
